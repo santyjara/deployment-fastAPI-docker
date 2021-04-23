e0ed1dea5cdfbf3e6f6d884653622f0c725d7c8a
Santiago Jaramillo González <37351232+santyjara@users.noreply.github.com>
2021-04-23 14:41:17 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,17 +19,3 @@ async def create_item(item: Item):
     pred = model.predict([[item.feature_1, item.feature_2]])[0]
 
     return {'prediction': int(pred)}
-
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: str = None):
-#     return {"item_id": item_id, "q": q}
